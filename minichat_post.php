2586e4acabc81546f5f47c61792eff29da62af6d
Pierrick Boucharlat <p.boucharlat@agencepbweb.com>
2020-06-20 16:16:09 +0200

    Connection à la BDD

--- minichat_post.php
+++ minichat_post.php
@@ -6,5 +6,18 @@
 </head>
 <body>
 
+<?php
+// 	Connection à la base de données
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=test' .';port=' . 3308, 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}
+catch (Exception $e)
+{
+	die('Erreur : '. $e ->getMessage());
+}
+
+?>
+
 </body>
 </html>
