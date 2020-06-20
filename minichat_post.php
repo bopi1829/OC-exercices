358f776c80960d9a3e17aa0cb0bf31b9f0866889
Pierrick Boucharlat <p.boucharlat@agencepbweb.com>
2020-06-20 16:19:18 +0200

    Requête pour insérer le message

--- minichat_post.php
+++ minichat_post.php
@@ -17,6 +17,12 @@ catch (Exception $e)
 	die('Erreur : '. $e ->getMessage());
 }
 
+// Effectuer ici la requête qui insère le messsage
+$reponse = $bdd -> prepare('INSERT INTO minichat (pseudo, message) VALUES (?, ?)');
+$req = $reponse -> execute(array($_POST['pseudo'], $_POST['message']));
+
+
+
 ?>
 
 </body>
