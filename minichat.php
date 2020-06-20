e53c13261d1312adc7a7e082d7d68522df14d9f1
Pierrick Boucharlat <p.boucharlat@agencepbweb.com>
2020-06-20 11:29:54 +0200

    Création minichat

---
+++ minichat.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8" />
+	<title>Mini chat</title>
+</head>
+<body>
+	<form method = "post" action = "minichat_post.php">
+		<p><label>Pseudo</label> : <input type = "text" name = "pseudo"></p>
+		<p><label>Message</label> : <input type = "text" name = "message"></p>
+		<input type = "submit" value = "Envoyer" name = "Valider">
+	</form>
+
+</body>
+</html>
