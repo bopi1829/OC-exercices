0d404a608b93122602c9d20468265c70fe7118d4
bopi1829 <p.boucharlat@agencepbweb.com>
2020-08-11 17:07:47 +0200

    TP Blog

---
+++ Blog/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="public/css/style.css">
+	<title>Blog</title>
+</head>
+    <body>
+        <h1>Mon super blog !</h1>
+
+        <p>Derniers billets du blog :</p>
+        <?php
+        // Connexion à la BDD
+        try
+        {
+        $bdd = new PDO('mysql:host=localhost;dbname=test' . ';port=' . 3308, 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        }
+        catch (Exception $e)
+        {
+            die ('Erreur : ' .$e -> getMessage());
+        }
+
+
+        ?>
+    </body>
+</html>
