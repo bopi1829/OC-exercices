24012603112cc0f5948b4b104fa3604567c3beac
bopi1829 <p.boucharlat@agencepbweb.com>
2020-08-11 17:25:46 +0200

    Récupération des billets

--- Blog/index.php
+++ Blog/index.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
 	<meta charset="utf-8" />
-    <link rel="stylesheet" type="text/css" href="public/css/style.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
 	<title>Blog</title>
 </head>
     <body>
@@ -20,6 +20,24 @@
             die ('Erreur : ' .$e -> getMessage());
         }
 
+        // Récupération des billets
+        $req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh/%imin/%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
+        while ($donnees = $req->fetch())
+        {
+            ?>
+            <div class="news">
+            <h3><?php echo htmlspecialchars($donnees['titre']); ?>
+            <em> le <?php echo $donnees['date_creation_fr'];?></em>
+            </h3>
+            
+            <p>
+            <?php echo nl2br(htmlspecialchars($donnees['contenu'])); ?>
+            <br/>
+            <em><a href="commentaires.php?=billet=<?php echo $donnees['id']; ?>">Commentaires</a></em>
+            </p>
+            </div>
+            <?php
+        }
 
         ?>
     </body>
