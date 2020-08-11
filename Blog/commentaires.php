5c48a12fffbb2827e916b8d476ca807b6fec0c48
bopi1829 <p.boucharlat@agencepbweb.com>
2020-08-11 18:12:07 +0200

    Fin du TP

--- Blog/commentaires.php
+++ Blog/commentaires.php
@@ -38,6 +38,21 @@
         <h2>Commentaires</h2>
         <?php
         $req->closeCursor();
+
+        //Récupération des messages
+        $req = $bdd->prepare('SELECT id, auteur, commentaire, DATE_FORMAT(date_commentaire, \'%d/%m/%Y à %Hh%imin%ss\') AS date_commentaire_fr FROM commentaires WHERE id_billet = ? ORDER BY date_commentaire');
+        $req->execute(array($_GET['billet']));
+        while ($donnees = $req->fetch())
+        {
+        ?>
+        <p>
+            <strong><?php echo htmlspecialchars($donnees['auteur']); ?></strong> le <?php echo $donnees['date_commentaire_fr']; ?>
+        </p>
+        <p><?php echo nl2br(htmlspecialchars($donnees['commentaire'])); ?>
+        </p>
+        <?php
+        }
+        
         ?>
     </body>
 </html>
