e872c8a88f3e86fddb432802a9fd74db30f1526e
bopi1829 <p.boucharlat@agencepbweb.com>
2020-08-17 17:55:15 +0200

    Initial commit

---
+++ EspaceMembres/pageconnexion.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+
+	<meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="style.css">
+	<title>Espace Membres</title>
+
+</head>
+    <body>
+        
+    </body>
+</html>
---
+++ EspaceMembres/pageinscription.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+
+	<meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="style.css">
+	<title>Espace Membres</title>
+
+</head>
+    <body>
+
+        <h1>Inscription</h1>
+        
+        <div class="form">
+            <            
+        </div>
+        
+    </body>
+</html>
